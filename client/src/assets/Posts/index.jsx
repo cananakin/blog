82005469b14e6986acbad0b04cc00107b03fc0ff
Canan Akin <e.cananakin@gmail.com>
2025-10-18 23:54:22 +0100

    add event service, query service and moderation service

--- client/src/assets/Posts/index.jsx
+++ client/src/assets/Posts/index.jsx
@@ -7,7 +7,8 @@ export default function List() {
     const [posts, setPosts ] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
+        
         setPosts(res.data);
     }
 
@@ -26,7 +27,7 @@ export default function List() {
                     <h3>
                         {post.title}
                     </h3>
-                    <CommentList postId={post.id} />
+                    <CommentList comments={post.comments} />
                     <AddComment postId={post.id} />
                 </div>
             </div>
